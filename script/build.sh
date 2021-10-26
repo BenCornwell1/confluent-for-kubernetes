31c2f7378a123584fa40f67206b4f70496ca557c
Ben Cornwell <ben.cornwell@uk.ibm.com>
2021-10-26 23:39:16 +0100

    Updates

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./deploy.sh cfk5 mycluster-lon06-b3c-16x64-403315c8b53cfaaf40d7fd4ee4d91267-0000.eu-gb.containers.appdomain.cloud config.yaml kafka Passw0rd admin Passw0rd
+./deploy.sh cfk7 mycluster-lon06-b3c-16x64-403315c8b53cfaaf40d7fd4ee4d91267-0000.eu-gb.containers.appdomain.cloud config.yaml kafka Passw0rd admin Passw0rd
